e48ed4abe6e7a985039e29e650f44980c59642a0
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-02-16 00:07:10 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = [];
+  excuse.push(who[Math.floor(Math.random() * who.length)]);
+  excuse.push(action[Math.floor(Math.random() * action.length)]);
+  excuse.push(what[Math.floor(Math.random() * what.length)]);
+  excuse.push(when[Math.floor(Math.random() * when.length)]);
+  //console.log(excuse);
+  let text = excuse.join(" ");
+  document.getElementById("excuse").innerHTML = text;
+  //console.log("Hello Rigo from the console!");
 };
